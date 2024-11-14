826fc8c97d29204d7af9fdb66f4540cfee378ebb
antoine-perfettini <antoine.perfettini@edu.esiee.fr>
2024-11-14 10:55:58 +0000

    Mon code pour savoir si un mot est un palindrome

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+"""
+Code principale qui permet de déterminer si un mot est un palindrome 
+"""
 #### Fonction secondaire
-
-
 def ispalindrome(p):
-
+    """
+    Fonction secondaire
+    """
     # votre code ici
-    
-    return False
-
+    p = p.lower()
+    l = str.maketrans('éèàùêôëç','eeaueoec')
+    p = p.replace(' ','')
+    p = p.translate(l)
+    v = ''
+    for _, j in enumerate(p):
+        if j.isalnum():
+            v+=j
+    print(v)
+    if v != v[::-1]:
+        return False
+    print(v)
+    return True
 #### Fonction principale
-
-
 def main():
-
+    """
+    Fonction principale
+    """
     # vos appels à la fonction secondaire ici
-
+    print('voiture', ispalindrome('voiture'))
+    print('anticonstitutionnellement', ispalindrome('anticonstitutionnellement'))
+    print('sos', ispalindrome('sos'))
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
